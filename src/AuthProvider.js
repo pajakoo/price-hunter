e2de687f9273d96a66031c5d8808d835ec70d438
K.G <georgievkn82@gmail.com>
2023-12-19 15:00:19 +0200

    fix auth

--- src/AuthProvider.js
+++ src/AuthProvider.js
@@ -5,46 +5,33 @@ import axios from 'axios';
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [user, setUser] = useState(null);
-  const [url, setUrl] = useState('https://super-polo-shirt-tick.cyclic.app');// useState('http://localhost:3333');//
+  const [url, setUrl] = useState('https://super-polo-shirt-tick.cyclic.app');//  useState('http://localhost:3333');//
 
   const login = () => {
     window.open(`${url}/auth/google`, "_self")
   };
 
-  const logout = async () => {
-    setIsAuthenticated(false);
-    axios.get(`${url}/auth/logout`, {
-      withCredentials: true
-    }).then((res) => {
-      if (res.data === "done") {
-        // window.location.href = "/"
-      }
-    })
-  };
+	const logout = () => {
+		window.open(`${url}/auth/logout`, "_self");
+	};
 
-
+  const getUser = async () => {
+		try {
+			const { data } = await axios.get(`${url}/auth/login/success`, { withCredentials: true });
+			setUser(data.user._json);
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
   useEffect(() => {
-    const getUser = async () => {
-      try {
-        const response = await axios.get(`${url}/auth/login/success`, {
-          withCredentials: true,
-        });
-        if (response.data.message === "successfull") {
-          setIsAuthenticated(true);
-        }
-      } catch (err) {
-        console.log('refresh-pajak');
-        //window.location.reload();
-      }
-    };
-    getUser();
-  }, []);
+		getUser();
+	}, []);
+
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, user, login, logout }}>
+    <AuthContext.Provider value={{ user, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
--- src/components/App.js
+++ src/components/App.js
@@ -8,132 +8,54 @@ import Client from './Client';
 import Admin from './Admin';
 import UserManagement from './UserManagement';
 import { useAuth } from '../AuthProvider'
-// let deferredPrompt;
+let deferredPrompt;
 
-// window.addEventListener('beforeinstallprompt', (event) => {
-//   // Prevent the default prompt
-//   event.preventDefault();
-//   // Store the event for later use
-//   deferredPrompt = event;
-//   // Optionally show your own install button
-//   showInstallButton();
-// });
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Prevent the default prompt
+  event.preventDefault();
+  // Store the event for later use
+  deferredPrompt = event;
+  // Optionally show your own install button
+  showInstallButton();
+});
 
-// function showInstallButton() {
-//   const installButton = document.getElementById('installButton');
-//   installButton.style.display = 'block';
-//   installButton.addEventListener('click', () => {
-//     // Trigger the installation prompt
-//     deferredPrompt.prompt();
-//     // Wait for the user to respond to the prompt
-//     deferredPrompt.userChoice.then((choiceResult) => {
-//       if (choiceResult.outcome === 'accepted') {
-//         console.log('User accepted the install prompt');
-//       } else {
-//         console.log('User dismissed the install prompt');
-//       }
-//       // Clear the deferredPrompt variable
-//       deferredPrompt = null;
-//     });
-//   });
-// }
+function showInstallButton() {
+  const installButton = document.getElementById('installButton');
+  installButton.style.display = 'block';
+  installButton.addEventListener('click', () => {
+    // Trigger the installation prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+      // Clear the deferredPrompt variable
+      deferredPrompt = null;
+    });
+  });
+}
 
-// if (window.matchMedia('(display-mode: standalone)').matches) {
-//   // The app is already installed
-//   document.getElementById('installButton').style.display = 'none';
-// }
+if (window.matchMedia('(display-mode: standalone)').matches) {
+  // The app is already installed
+  document.getElementById('installButton').style.display = 'none';
+}
 
 
 
 function App() {
 
-  const [currentUser, setCurrentUser] = useState({});
-  const [url, setUrl] = useState('https://super-polo-shirt-tick.cyclic.app');//useState('http://localhost:3333');//
-  const { isAuthenticated, user, login, logout } = useAuth();
+  const [url, setUrl] = useState('https://super-polo-shirt-tick.cyclic.app'); //useState('http://localhost:3333');//
+  const { user, login, logout } = useAuth();
  
-  async function fetchUserInfoFromServer(response) {
-    var user = jwt_decode(response.credential);
-
-    try {
-      const r = await fetch(`${url}/api/userInfo`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ user }),
-      });
-
-      const userInfo = await r.json();
-      // console.log(userInfo);
-      setCurrentUser(userInfo);
-
-    } catch (error) {
-      console.error('Грешка при извличане на информация за потребителя:', error.message);
-      throw error;
-    }
-  }
-
-  // useEffect(() => {
-  //   // Fetch user data from the server
-  //     axios.get(`${url}/user`).then((response) => {
-  //       setCurrentUser(response.data);
-  //     });
-
-  // }, []);
-
-
-  useEffect(() => {
-    /* global google */
-    // google.accounts.id.initialize({
-    //   client_id: '698649535640-7j0jm7jlscolg3gfdr7dkn0qs248jeep.apps.googleusercontent.com',
-    //   callback: fetchUserInfoFromServer
-    // });
-
-    // google.accounts.id.renderButton(
-    //   document.getElementById("signInDiv"),
-    //   { theme: "outline", size: "large" }
-    // );
-
-  }, []);
-
-
-  useEffect(() => {
-    console.log('gg',user, isAuthenticated);
-  })
-
-  const handleSignOut = () => {
-    setCurrentUser(null);
-    // google.accounts.id.renderButton(
-    //   document.getElementById("signInDiv2"),
-    //   { theme: "outline", size: "large" }
-    // );
-  };
-
   const checkUserRights = (roles) => {
     // Check if the user has the 'admin' ro
     //console.log('right', currentUser.roles);
     //return currentUser && currentUser.roles.includes(roles);
     return true;
   };
-
-
-  const Home = () => {
-    return (
-      <div>
-        <div className="home" onClick={() => window.open("http://localhost:3333/auth/google", "_self")} >
-          pajak + ${currentUser.displayName || ''}
-        </div>
-        <div className="home" onClick={() => window.open("http://localhost:3333/auth/logout", "_self")} >
-          logout
-        </div>
-
-      </div>
-
-
-    )
-  }
-
-
   const Login = () => {
     return (
       <div>
@@ -144,15 +66,6 @@ function App() {
   };
 
   const Logout = () => {
-    const handleLogout = async () => {
-      try {
-        await fetch(`${url}/auth/logout`, { method: 'GET' });
-        window.location.href = '/';
-      } catch (error) {
-        console.error('Error logging out:', error);
-      }
-    };
-
     return (
       <div>
         <button className="btn btn-link nav-link" onClick={logout}>
@@ -162,19 +75,16 @@ function App() {
     );
   };
 
-
   const PrivateRoutes = () => {
     return (
-      isAuthenticated ? <Outlet /> : <Navigate to="/login" />
+      user ? <Outlet /> : <Navigate to="/login" />
     )
   }
-
-
 
   return (
     <>
       <Router>
-        {/* <button id="installButton">Install App</button> */}
+        <button id="installButton">Install App</button>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container">
             <ul className="navbar-nav">
@@ -183,11 +93,6 @@ function App() {
                   Home
                 </Link>
               </li>
-              {/* <li className="nav-item">
-                <Link className="nav-link" to="/">
-                  Client
-                </Link>
-              </li> */}
               <li className="nav-item">
                 <Link className="nav-link" to="/admin">
                   Admin
@@ -215,7 +120,7 @@ function App() {
             <Route element={<Admin />} path="/admin" />
             <Route element={<UserManagement />} path="/users" />
           </Route>
-          {isAuthenticated ? '' : <Route element={<Login />} path="/login" /> }
+          { user ? '' : <Route element={<Login />} path="/login" /> }
         </Routes>
       </Router>
 
